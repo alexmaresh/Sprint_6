84e4e75fdbf71cc7f35db9b044d195488570af2a
Alexandra Maresh <maresh.al@yandex.ru>
2024-11-23 02:44:40 +0300

    move url to settings

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium import webdriver
+from settings import TestSettings
 
 
 @pytest.fixture
 def driver():
     driver = webdriver.Firefox()
-    driver.get("https://qa-scooter.praktikum-services.ru/")
+    driver.get(TestSettings.base_url)
     yield driver
     driver.quit()
